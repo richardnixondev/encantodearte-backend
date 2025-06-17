47e9f9c96d2104ed49cca035d53da89d52286657
Richard Nixon <rnixonaf@gmail.com>
2025-06-17 21:33:03 +0100

    Setup express, mongoose, connecting local mongo and create user model close#1

--- models/User.model.js
+++ models/User.model.js
@@ -1,29 +1,52 @@
-const { Schema, model } = require("mongoose");
+const mongoose = require ('mongoose');
+const { Schema, model } = mongoose;
 
-// TODO: Please make sure you edit the User model to whatever makes sense in this case
-const userSchema = new Schema(
-  {
-    email: {
-      type: String,
-      required: [true, "Email is required."],
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
+const BRAZILIAN_STATES = [
+  'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO',
+  'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI',
+  'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'
+];
+
+const addressSchema = new mongoose.Schema({
+  fullName: {type: String, required: true},
+  street: {type: String, required: true},
+  number: {type: String, required: true},
+  complement: String,
+  city: {type: String, required: true},
+  state: {
+    type: String,
+    enum: BRAZILIAN_STATES,
+    required: true
+  },
+  zipcode: {type: String, required: true},
+  country: {
+    type: String,
+    default: 'Brasil',
+    enum: ['Brasil'],
+    required: true
+  },
+  phone: { type: String, required: true},
+  isDefault: { type: Boolean, default: false}
+
+});
+
+const userSchema = new mongoose.Schema ({
+  surname: {type: String, required: true},
+  lastname: {type: String, required: true},
+  email: {
+    type: String, 
+    required: [true, "Email is required."],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
       type: String,
       required: [true, "Password is required."],
     },
-    name: {
-      type: String,
-      required: [true, "Name is required."],
-    },
-  },
-  {
-    // this second object adds extra properties: `createdAt` and `updatedAt`
-    timestamps: true,
-  }
-);
+  addresses: [addressSchema] 
+}, { timestamps: true });
+
 
 const User = model("User", userSchema);
 
