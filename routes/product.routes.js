92ec5c7791094e470e9fa27cd82335fea3588e3f
Richard Nixon <rnixonaf@gmail.com>
2025-06-20 16:40:48 +0100

    Added  more routers for product

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Product = require('../models/Product.model');
 const { isAuthenticated } = require {'../middleware/jwt.middleware'};
 
-//get all products
+//Get all products
 router.get('/', async (req, res) => {
     try {
         const products = await Product.find().sort({ createdAt: -1 });
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-// Create New Product (private route)
+//Get Product by ID
 router.get('/:productId', async (req, res) => {
     try {
         const product = await Product.findById(req.params.productId);
@@ -26,3 +26,56 @@ router.get('/:productId', async (req, res) => {
         res.status(500).json({ message: 'Error fetching product', error: err});
     }
 });
+
+// Post create New Product (private router)
+router.post('/', isAuthenticated, async (req, res) => {
+    try {
+        const {name, description, category, price, stock, imageUrl } = req.body;
+    
+        const newProduct = await Product.create({
+            name,
+            description,
+            categoty,
+            price,
+            stock,
+            imageUrl,
+            createdBy: req.payload._id //from jwt auth token
+        });
+
+        res.status(201).json(newProduct);
+    } catch (err) {
+        res.status(400).json({ message: 'Error creating product', error: err });
+    }
+});
+
+// Put update product by ID (private router)
+router.put('/:productId', isAuthenticated, async(req, res) => {
+    try{
+        const updatedProduct = await Product.findByIdAndUpdate({
+            req.params.productId,
+            req.body,
+            { new: true, runValidators: true }
+        );
+        if (!updatedProduct) {
+            return res.status(404).json({ message: 'Error updateing product', error: err });
+        }
+        res.status(200).json(updateProduct);
+  } catch (err) {
+        res.status(400).json({ message: 'Error updating', error: err});
+  }
+});
+
+//Delete product by ID (private router)
+router.delete('/:productId', isAuthenticated, async (req, res) => {
+    try {
+        const deletedProduct = await Product.findByIdAndDelete(req, res) => {
+        if (!deletedProduct) {
+            return res.status(404).json({ message: 'Product now found' });
+        }
+        res.status(200)/.json({'produc deleted successfully'})
+     } catch (err) {
+        res.status.(500).json({ message: 'Error deleting product', error: err });
+    } 
+});
+
+module.exports = router;
