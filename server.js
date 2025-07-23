642e627a62c0c6735942bdf8368d41e675e688da
Richard Nixon <rnixonaf@gmail.com>
2025-07-23 15:53:40 +0100

    fix deployment

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const path = require("path");
-const express = require("express");
 const app = require("./app");
 
 // ℹ️ Sets the PORT from .env or defaults to 5005
 const PORT = process.env.PORT || 5005;
-
-// Serve static files from React/Vite build folder in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client", "dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-  });
-}
 
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`);
